84f36617e0f92b3bb5a16ec9c54e89b023520832
Karthick Raja S <karthickrajas.1123@gmail.com>
2026-05-27 16:45:10 +0530

    fixed messages page

--- src/hooks/useMessages.js
+++ src/hooks/useMessages.js
@@ -5,7 +5,7 @@ function useMessages () {
 
     useEffect(() => {
 
-        fetch(fetch("https://react-portfolio-f4ed.onrender.com/messages?password=kr123")) 
+        fetch("https://react-portfolio-f4ed.onrender.com/messages?password=kr123") 
             .then(res => res.json())
             .then(data => setMessages(data))    //stores data to display in UI
             .catch(err => console.error(err));
